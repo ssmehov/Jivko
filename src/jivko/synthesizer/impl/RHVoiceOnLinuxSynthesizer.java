bcdccf2645a39a175eff19649640e98ed1ccd194
Владимир <shark@toughshark.(none)>
2013-09-03 20:58:58 +0300

    RHVoice for Linux fix

--- src/jivko/synthesizer/impl/RHVoiceOnLinuxSynthesizer.java
+++ src/jivko/synthesizer/impl/RHVoiceOnLinuxSynthesizer.java
@@ -11,40 +11,54 @@ import jivko.synthesizer.Synthesizer;
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 
+import jivko.util.MakeSound;
+
 /**
  *
  * @author Sergii Smehov (smehov.com)
  */
 public class RHVoiceOnLinuxSynthesizer implements Synthesizer {
 
-  @Override
-  public void talk(String text) {
-    
-    if (text == null)
-      return;
-    
-    try {
-      String TEXT_TO_SAY_FILENAME = "textToSay.txt";
-      String RESULT_WAV_FILENAME = "out.wav";
-      
-      PrintWriter writer = new PrintWriter(TEXT_TO_SAY_FILENAME, "UTF-8");
-      writer.println(text);
-      writer.close();
-      
-      Process p = Runtime.getRuntime().exec(new String[]{"bash", "-c", "RHVoice -i "+ TEXT_TO_SAY_FILENAME + " -o " + RESULT_WAV_FILENAME});
-      
-      playWav(RESULT_WAV_FILENAME);
-      
-    } catch (Exception ex) {
-      Logger.getLogger(RHVoiceOnLinuxSynthesizer.class.getName()).log(Level.SEVERE, null, ex);
-    }
-  }
-  
-  public static void playWav(String fileName) throws Exception {        
-    InputStream in = new FileInputStream(fileName);
-    
-    AudioStream audioStream = new AudioStream(in);
+    @Override
+    public void talk(String text) {
 
-    AudioPlayer.player.start(audioStream);
-  }
+        if (text == null || text == "") {
+            return;
+        }
+
+        try {
+            String TEXT_TO_SAY_FILENAME = "textToSay.txt";
+            String RESULT_WAV_FILENAME = "out.wav";
+
+            PrintWriter writer = new PrintWriter(TEXT_TO_SAY_FILENAME, "UTF-8");
+            writer.println(text);
+            writer.close();
+
+            Process p = Runtime.getRuntime().exec(new String[]{"bash", "-c", "RHVoice -i " + TEXT_TO_SAY_FILENAME + " -o " + RESULT_WAV_FILENAME});
+            p.waitFor();
+            
+            int exitCode = p.exitValue();
+
+            if (exitCode == 0) {
+              playWav(RESULT_WAV_FILENAME);
+            } else {
+                throw new Exception("cann't run RHVoice");
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void playWav(String fileName) throws Exception {
+        MakeSound.playSound(fileName);
+    }
+
+    public static void playWav2(String fileName) throws Exception {
+        InputStream in = new FileInputStream(fileName);
+
+        AudioStream audioStream = new AudioStream(in);
+
+        AudioPlayer.player.start(audioStream);
+    }
 }
---
+++ src/jivko/util/MakeSound.java
@@ -0,0 +1,60 @@
+package jivko.util;
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class MakeSound {
+
+    private static final int BUFFER_SIZE = 128000;
+    private static File soundFile;
+    private static AudioInputStream audioStream;
+    private static AudioFormat audioFormat;
+    private static SourceDataLine sourceLine;
+
+    /**
+     * @param filename the name of the file that is going to be played
+     */
+    public static void playSound(String filename) {
+
+        String strFilename = filename;
+
+        try {
+            soundFile = new File(strFilename);
+            audioStream = AudioSystem.getAudioInputStream(soundFile);
+            audioFormat = audioStream.getFormat();
+
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+            sourceLine.open(audioFormat);
+
+            sourceLine.start();
+
+            int nBytesRead = 0;
+            byte[] abData = new byte[BUFFER_SIZE];
+            while (nBytesRead != -1) {
+                try {
+                    nBytesRead = audioStream.read(abData, 0, abData.length);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (nBytesRead >= 0) {
+                    @SuppressWarnings("unused")
+                    int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+                }
+            }
+
+            sourceLine.drain();
+            sourceLine.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
