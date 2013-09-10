3f74b0be732fca7db693d54fc5b399cc127681ad
Vladimir Shark <shark@toughshark.(none)>
2013-09-10 04:01:30 +0400

    run.sh added

---
+++ run.sh
@@ -0,0 +1,2 @@
+java -classpath './lib/cgjsapi.jar:./lib/JavaGoogleSpeachAPi.jar:./lib/javaFlacEncoder-0.3.1.jar:./lib/sphinx4.jar:./lib/stateless4j-1.0.0.jar:./lib/RXTXcomm.jar:./build/classes' 'jivko.Main'
+
