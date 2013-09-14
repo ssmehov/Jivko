633a33686fbc78368e016d44d48f3208f74641ed
Sergii Smehov <info@smehov.com>
2013-09-14 16:01:53 +0400

    Text say ability

--- src/jivko/sphinx/TextEngine.java
+++ src/jivko/sphinx/TextEngine.java
@@ -3,26 +3,35 @@ package jivko.sphinx;
 import jivko.brain.Brain;
 import jivko.synthesizer.SynthesizerFactory;
 
-
 public class TextEngine extends Thread implements Runnable {
-    //public BlockingQueue<Command> queue = new LinkedBlockingQueue<>();    
-            
-    @Override
-    public void run() {
-      TextFrontend textFrontend = new TextFrontend();
-      while (true) {        
-        try {
-          UtteranceData utterance = textFrontend.getData();
-          String utteranceStr = utterance.getUtterence();
-          String answer = Brain.getInstance().findAnswer(utteranceStr);
-          SynthesizerFactory.getSynthesizer().talk(answer);
-        } catch (Exception ex) {
-          ex.printStackTrace();          
-        }          
+  //public BlockingQueue<Command> queue = new LinkedBlockingQueue<>();    
+
+  public static final String TEXT_MARKER = "Т:";
+  
+  @Override
+  public void run() {
+    TextFrontend textFrontend = new TextFrontend();
+    while (true) {
+      try {
+        UtteranceData utterance = textFrontend.getData();
+        String utteranceStr = utterance.getUtterence();
+
+        String answer;
+
+        if (utteranceStr.startsWith(TEXT_MARKER)) {
+          answer = utteranceStr.substring(TEXT_MARKER.length());
+        } else {
+          answer = Brain.getInstance().findAnswer(utteranceStr);
+        }
+
+        SynthesizerFactory.getSynthesizer().talk(answer);
+      } catch (Exception ex) {
+        ex.printStackTrace();
       }
     }
-
+  }
   static TextEngine textEngine = new TextEngine();
+
   public static TextEngine getInstance() {
     return textEngine;
   }
